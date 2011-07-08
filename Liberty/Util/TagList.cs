0e2fda43a38ffb9f0ec826a1444fa6df1bcd1d5b
amd64_cp <SND\amd64_cp>
2011-07-08 15:39:49 +0000

    Improved taglist support, added some new tags to the taglist, fixed an issue with bipd swap disabling, and fixed the "you must select a valid gamesave" dialog constantly reappearing

--- Liberty/Util/TagList.cs
+++ Liberty/Util/TagList.cs
@@ -12,14 +12,24 @@ namespace Liberty.Util
     /// </summary>
     class TagList
     {
+        /// <summary>
+        /// Loads a taglist from a file.
+        /// </summary>
+        /// <param name="path">The file to load from</param>
+        /// <returns>A TagList object representing the file contents</returns>
         public static TagList FromFile(string path)
         {
-            return new TagList(File.OpenRead(path));
+            return new TagList(new StreamReader(File.OpenRead(path)));
         }
 
+        /// <summary>
+        /// Loads a taglist stored in a string.
+        /// </summary>
+        /// <param name="data">The string to read from</param>
+        /// <returns>A TagList object representing the string contents</returns>
         public static TagList FromString(string data)
         {
-            return new TagList(new MemoryStream(System.Text.ASCIIEncoding.Default.GetBytes(data)));
+            return new TagList(new StringReader(data));
         }
 
         /// <summary>
@@ -43,52 +53,56 @@ namespace Liberty.Util
             public uint id;
         }
         
-        private TagList(Stream stream)
-        {
-            StreamReader reader = new StreamReader(stream);
+        private TagList(TextReader reader)
+        {   
             bool inBlock = false;
             string currentGroup = "";
-            while (!reader.EndOfStream)
+            string line = reader.ReadLine();
+            while (line != null)
             {
-                string line = reader.ReadLine().Trim();
-                if (line.Length == 0 || line.StartsWith(";"))
-                    continue;
-
-                if (line.StartsWith("[") && line.EndsWith("]"))
+                int commentPos = line.IndexOf(';');
+                if (commentPos != -1)
+                    line = line.Substring(0, commentPos);
+                line = line.Trim();
+                if (line.Length > 0)
                 {
-                    // Enter a new block
-                    inBlock = true;
-                    currentGroup = line.Trim(new char[3] { '[', ']', ' ' }).ToLower();
-                    if (currentGroup.Length == 0)
-                        throw new ArgumentException("The taglist is invalid.\r\nGroup names must contain at least one non-whitespace character.");
-                }
-                else if (inBlock)
-                {
-                    // Read a key = value pair
-                    int equalsPos = line.IndexOf('=');
-                    if (equalsPos == -1)
-                        throw new ArgumentException("The taglist is invalid.\r\nExpressions in a block must be key = value pairs.");
-                    string idStr = line.Substring(0, equalsPos).TrimStart(null);
-                    string name = line.Substring(equalsPos + 1).Trim();
-                    
-                    // Parse it
-                    uint id;
-                    if (idStr.StartsWith("0x"))
+                    if (line.StartsWith("[") && line.EndsWith("]"))
                     {
-                        // Parse a hex number
-                        if (!uint.TryParse(idStr.Substring(2), NumberStyles.HexNumber, null, out id))
-                            throw new ArgumentException("The taglist is invalid.\r\nKeys starting with 0x must be valid hex numbers.");
+                        // Enter a new block
+                        inBlock = true;
+                        currentGroup = line.Trim(new char[3] { '[', ']', ' ' }).ToLower();
+                        if (currentGroup.Length == 0)
+                            throw new ArgumentException("The taglist is invalid.\r\nGroup names must contain at least one non-whitespace character.");
                     }
-                    else
+                    else if (inBlock)
                     {
-                        // Parse a decimal number
-                        if (!uint.TryParse(idStr, out id))
-                            throw new ArgumentException("The taglist is invalid.\r\nKeys with no prefix must be valid decimal integers.");
-                    }
+                        // Read a key = value pair
+                        int equalsPos = line.IndexOf('=');
+                        if (equalsPos == -1)
+                            throw new ArgumentException("The taglist is invalid.\r\nExpressions in a block must be key = value pairs.");
+                        string idStr = line.Substring(0, equalsPos).TrimStart(null);
+                        string name = line.Substring(equalsPos + 1).TrimStart(null);
 
-                    // Store it
-                    _tags[new TagKey { group = currentGroup, id = id }] = name;
+                        // Parse it
+                        uint id;
+                        if (idStr.StartsWith("0x"))
+                        {
+                            // Parse a hex number
+                            if (!uint.TryParse(idStr.Substring(2), NumberStyles.HexNumber, null, out id))
+                                throw new ArgumentException("The taglist is invalid.\r\nKeys starting with 0x must be valid hex numbers.");
+                        }
+                        else
+                        {
+                            // Parse a decimal number
+                            if (!uint.TryParse(idStr, out id))
+                                throw new ArgumentException("The taglist is invalid.\r\nKeys with no prefix must be valid decimal integers.");
+                        }
+
+                        // Store it
+                        _tags[new TagKey { group = currentGroup, id = id }] = name;
+                    }
                 }
+                line = reader.ReadLine();
             }
             reader.Close();
         }
