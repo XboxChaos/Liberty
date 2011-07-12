7d643c3cdc6af4252d7d86491d0f74c66f4a4386
Alex Forbes-Reed <github@alx.red>
2011-07-12 19:59:50 +0000

    Added base for Settings UI

--- Liberty/Controls/settingsMain.xaml.cs
+++ Liberty/Controls/settingsMain.xaml.cs
@@ -19,9 +19,91 @@ namespace Liberty.Controls
     /// </summary>
     public partial class settingsMain : UserControl
     {
+        private bool isApp = true;
+        BrushConverter bc = new BrushConverter();
+        FontWeight fw = new FontWeight();
+
         public settingsMain()
         {
             InitializeComponent();
+
+            themePanel.Visibility = System.Windows.Visibility.Hidden;
+            themePanel.Visibility = System.Windows.Visibility.Visible;
         }
+
+        #region uncleanWPFstuff
+        
+        #region btnSoftwpf
+        private void btnSoft_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!isApp)
+                btnSoft.Foreground = (Brush)bc.ConvertFrom("#FFD4D2D2");
+        }
+
+        private void btnSoft_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!isApp)
+                btnSoft.Foreground = (Brush)bc.ConvertFrom("#FF868686");
+        }
+
+        private void btnSoft_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!isApp)
+               btnSoft.Foreground = (Brush)bc.ConvertFrom("#FFD4D2DF");
+        }
+
+        private void btnSoft_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!isApp)
+            {
+                btnSoft.Foreground = (Brush)bc.ConvertFrom("#FF000000");
+                btnSoft.FontWeight = FontWeights.ExtraBold;
+
+                btnTheme.FontWeight = FontWeights.Normal;
+                btnTheme.Foreground = (Brush)bc.ConvertFrom("#FF868686");
+            }
+
+            isApp = true;
+            softwarePanel.Visibility = System.Windows.Visibility.Visible;
+            themePanel.Visibility = System.Windows.Visibility.Hidden;
+        }
+        #endregion
+
+        #region btnSoftwpf
+        private void btnTheme_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isApp)
+                btnTheme.Foreground = (Brush)bc.ConvertFrom("#FFD4D2D2");
+        }
+
+        private void btnTheme_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isApp)
+                btnTheme.Foreground = (Brush)bc.ConvertFrom("#FF868686");
+        }
+
+        private void btnTheme_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (isApp)
+                btnTheme.Foreground = (Brush)bc.ConvertFrom("#FFD4D2DF");
+        }
+
+        private void btnTheme_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (isApp)
+            {
+                btnTheme.Foreground = (Brush)bc.ConvertFrom("#FF000000");
+                btnTheme.FontWeight = FontWeights.ExtraBold;
+
+                btnSoft.FontWeight = FontWeights.Normal;
+                btnSoft.Foreground = (Brush)bc.ConvertFrom("#FF868686");
+            }
+
+            isApp = false;
+            themePanel.Visibility = System.Windows.Visibility.Visible;
+            softwarePanel.Visibility = System.Windows.Visibility.Hidden;
+        }
+        #endregion
+        #endregion
     }
 }
