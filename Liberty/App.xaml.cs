b585527ce6c481985b47d85d30fed40d756c4a49
amd64_cp <SND\amd64_cp>
2011-07-13 15:19:45 +0000

    Added a dialog that pops up when the program is run and some of the DLL files are missing

--- Liberty/App.xaml.cs
+++ Liberty/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.IO;
 
 namespace Liberty
 {
@@ -12,5 +13,14 @@ namespace Liberty
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (File.Exists("X360.dll") || !File.Exists("MahApps.Metro.dll") || !File.Exists("MahApps.Metro.Controls.dll") || !File.Exists("System.Windows.Interactivity.dll"))
+            {
+                Controls.dllMissingError dllMissing = new Controls.dllMissingError();
+                dllMissing.ShowDialog();
+                this.Shutdown();
+            }
+        }
     }
 }
