61d1d86ffa5423d94ecf6ba824c0f56b84df430e
Alex Forbes-Reed <github@alx.red>
2011-07-20 17:42:56 +0000

    Firstrun edits.

--- Liberty/App.xaml.cs
+++ Liberty/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Windows;
 using System.IO;
 using Liberty.classInfo.storage.settings;
+using Microsoft.Win32;
+using System.Reflection;
 
 namespace Liberty
 {
@@ -16,6 +18,15 @@ namespace Liberty
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            RegistryKey keyApp = Registry.CurrentUser.CreateSubKey("Software\\Xeraxic\\Liberty\\");
+
+            // Update
+            string isFirstRun = (string)keyApp.GetValue("Version");
+            if (isFirstRun != Assembly.GetExecutingAssembly().GetName().Version.ToString())
+            {
+                keyApp.SetValue("Version", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            }
+
             classInfo.applicationExtra.loadApplicationSettings();
 
             if (applicationSettings.checkDLL && (!File.Exists("X360.dll") || !File.Exists("MahApps.Metro.dll") || !File.Exists("MahApps.Metro.Controls.dll") || !File.Exists("System.Windows.Interactivity.dll")))
