5d52d4efd05a2d5356e0606dd9ec30d1cd22481d
amd64_cp <SND\amd64_cp>
2011-07-23 22:30:24 +0000

    Added checks for Microsoft.Expression.Drawing.dll

--- Liberty/App.xaml.cs
+++ Liberty/App.xaml.cs
@@ -37,7 +37,7 @@ namespace Liberty
 
             classInfo.applicationExtra.loadApplicationSettings();
 
-            if (applicationSettings.checkDLL && (!File.Exists("X360.dll") || !File.Exists("MahApps.Metro.dll") || !File.Exists("MahApps.Metro.Controls.dll") || !File.Exists("System.Windows.Interactivity.dll")))
+            if (applicationSettings.checkDLL && (!File.Exists("X360.dll") || !File.Exists("MahApps.Metro.dll") || !File.Exists("MahApps.Metro.Controls.dll") || !File.Exists("System.Windows.Interactivity.dll") || !File.Exists("Microsoft.Expression.Drawing.dll")))
             {
                 Controls.dllMissingError dllMissing = new Controls.dllMissingError();
                 dllMissing.ShowDialog();
