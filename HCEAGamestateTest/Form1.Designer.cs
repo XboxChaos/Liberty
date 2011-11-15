b2b11f41d1a6bfc35084f4d42a13ea40ad44ff5a
Alex Forbes-Reed <github@alx.red>
2011-11-15 17:40:58 +0000

    Re-added vs2010 support. My bad.

--- HCEAGamestateTest/Form1.Designer.cs
+++ HCEAGamestateTest/Form1.Designer.cs
@@ -89,7 +89,7 @@
             this.groupBox5.Controls.Add(this.label10);
             this.groupBox5.Controls.Add(this.txtGameIdent);
             this.groupBox5.Controls.Add(this.label11);
-            this.groupBox5.Location = new System.Drawing.Point(340, 6);
+            this.groupBox5.Location = new System.Drawing.Point(340, 12);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(296, 120);
             this.groupBox5.TabIndex = 19;
@@ -230,7 +230,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(644, 334);
+            this.tabPage2.Size = new System.Drawing.Size(644, 288);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Weap Properties";
             this.tabPage2.UseVisualStyleBackColor = true;
