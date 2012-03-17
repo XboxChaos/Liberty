380a38fe474227229d85db3c9c3c829d15f9821f
Aaron <1750941+adierking@users.noreply.github.com>
2012-03-17 18:31:18 +0000

    Fixed critical bugs in the weapon editor and object editor, saves should work now but there are still some rather major bugs

--- Liberty/HReach/UI/editWeapons.xaml.cs
+++ Liberty/HReach/UI/editWeapons.xaml.cs
@@ -48,7 +48,7 @@ namespace Liberty.Controls
             _loading = true;
             BuildWeaponLists();
 
-            for (int i = 0; i < _weaponBoxes.Length; i++)
+            for (int i = 1; i < _weaponBoxes.Length; i++)
                 EnableComboBox(i, false);
 
             Reach.BipedObject playerBiped = _saveManager.SaveData.Player.Biped;
@@ -62,14 +62,19 @@ namespace Liberty.Controls
         public bool Save()
         {
             Reach.BipedObject playerBiped = _saveManager.SaveData.Player.Biped;
+
+            // We have to do the save in two passes:
+            // 1. Replace weapons and edit ammo
+            // 2. Drop weapons in reverse order
             for (int i = 0; i < _weaponBoxes.Length; i++)
             {
                 ComboBox box = _weaponBoxes[i];
                 Grid grid = (Grid)box.Tag;
-                if (grid.Children.Count > 0)
+                
+                // Replace the weapon if it does not match
+                ComboBoxItem item = (ComboBoxItem)box.SelectedItem;
+                if (item.Tag != null)
                 {
-                    // Replace the weapon if it does not match
-                    ComboBoxItem item = (ComboBoxItem)box.SelectedItem;
                     WeaponItem weapon = (WeaponItem)item.Tag;
                     if (weapon.Object != playerBiped.GetWeapon(i))
                     {
@@ -86,10 +91,22 @@ namespace Liberty.Controls
                             weapon.Object.Z = 0;
                         }
                     }
+                }
 
+                if (grid.Children.Count > 0)
+                {
                     // Save its ammo info
                     Util.IAmmoDisplay display = (Util.IAmmoDisplay)grid.Children[0];
                     display.Save();
+                }
+            }
+
+            for (int i = _weaponBoxes.Length - 1; i >= 0; i--)
+            {
+                if (_weaponBoxes[i].SelectedIndex == 0)
+                {
+                    // (nothing) item is selected
+                    playerBiped.DropWeapon(i);
                 }
             }
             return true;
@@ -107,9 +124,8 @@ namespace Liberty.Controls
 
         private void EnableComboBox(int index, bool enable)
         {
-            _weaponBoxes[index].IsEnabled = enable;
-            Grid grid = (Grid)_weaponBoxes[index].Tag;
-            grid.IsEnabled = enable;
+            UIElement parentGrid = (Grid)_weaponBoxes[index].Parent;
+            parentGrid.IsEnabled = enable;
         }
 
         private class WeaponItem : IComparable<WeaponItem>
@@ -143,10 +159,12 @@ namespace Liberty.Controls
 
             // Sort the weapons by name into a set
             Reach.CampaignSave saveData = _saveManager.SaveData;
+            Reach.BipedObject playerBiped = saveData.Player.Biped;
             SortedSet<WeaponItem> weapons = new SortedSet<WeaponItem>();
             foreach (Reach.GameObject obj in saveData.Objects)
             {
-                if (obj != null && obj.TagGroup == Reach.TagGroup.Weap && !obj.Deleted && (obj.Carrier == null || obj.Carrier.TagGroup != Reach.TagGroup.Vehi))
+                // Only process non-null weapons that aren't deleted, aren't carried by a vehicle, or that are carried by the player
+                if (obj != null && obj.TagGroup == Reach.TagGroup.Weap && !obj.Deleted && (obj.Carrier == null || obj.Carrier.TagGroup != Reach.TagGroup.Vehi || obj.Carrier == playerBiped))
                 {
                     string name = _tagList.Identify(obj, false);
                     if (name.StartsWith("Spartan"))
@@ -186,21 +204,33 @@ namespace Liberty.Controls
             }
         }
 
+        /// <summary>
+        /// Initializes a combo box with a specific weapon
+        /// </summary>
+        /// <param name="typeBox">The combo box to initialize</param>
+        /// <param name="weapon">The weapon to select in the combo box</param>
         private void LoadWeapon(ComboBox typeBox, Reach.WeaponObject weapon)
         {
             if (weapon != null)
-                typeBox.SelectedIndex = ChangeWeapon(typeBox, 0, _weaponIndices[weapon]);
+            {
+                int index = _weaponIndices[weapon];
+                ChangeWeapon(typeBox, 0, index);
+                typeBox.SelectedIndex = index;
+            }
             else
-                typeBox.SelectedIndex = ChangeWeapon(typeBox, 0, 0);
+            {
+                ChangeWeapon(typeBox, 0, 0);
+                typeBox.SelectedIndex = 0;
+            }
         }
 
         /// <summary>
-        /// Propagates a weapon selection change to all of the ComboBoxes.
+        /// Propagates a weapon selection change to all of the combo boxes.
         /// </summary>
         /// <param name="freeWeapons">The queue to add free weapons from</param>
         /// <param name="mapId">The map ID of the affected item</param>
         /// <param name="index">The index of the affected item</param>
-        /// <param name="ignore">The ComboBox to ignore when propagating changes</param>
+        /// <param name="ignore">The combo box to ignore when propagating changes</param>
         private void PropagateWeaponChange(SortedSet<int> freeWeapons, uint mapId, int index, ComboBox ignore)
         {
             /* The basic idea behind this is that if a weapon is not selected in
@@ -225,7 +255,7 @@ namespace Liberty.Controls
                     bool alreadyIn = false;
                     foreach (ComboBoxItem item in box.Items)
                     {
-                        if (item != null && item.Tag != null)
+                        if (item.Tag != null)
                         {
                             WeaponItem weapon = (WeaponItem)item.Tag;
                             if (weapon.Object.MapID == mapId && item.Visibility == Visibility.Visible)
@@ -255,7 +285,13 @@ namespace Liberty.Controls
             }
         }
 
-        private int ChangeWeapon(ComboBox typeBox, int oldIndex, int newIndex)
+        /// <summary>
+        /// Changes the selected weapon in a combo box.
+        /// </summary>
+        /// <param name="typeBox">The box to change</param>
+        /// <param name="oldIndex">The index of the old weapon</param>
+        /// <param name="newIndex">The index of the new weapon</param>
+        private void ChangeWeapon(ComboBox typeBox, int oldIndex, int newIndex)
         {
             Grid ammoGrid = (Grid)typeBox.Tag;
             ammoGrid.Children.Clear();
@@ -287,29 +323,20 @@ namespace Liberty.Controls
                 PropagateWeaponChange(freeWeapons, item.Object.MapID, newIndex, typeBox);
 
                 string name = _tagList.Identify(item.Object, true);
-                name = name.ToLower();
-
-                // Display the weapon details
-                UIElement display;
-                if (name.Contains("plasma") || name.Contains("energy") || name.Contains("laser"))
-                    display = new plasmaAmmoDisplay(item.Object);
-                else if (name.Contains("target") || name.Contains("locator"))
-                    display = new targetLocatorDisplay(_saveManager.SaveData);
-                else
-                    display = new regularAmmoDisplay(item.Object);
-                ammoGrid.Children.Add(display);
+                ammoGrid.Children.Add(Reach.WeaponEditing.GetAmmoDisplay(_saveManager.SaveData, item.Object, name));
 
                 EnableComboBox(boxIndex, true);
                 EnableNextComboBox(boxIndex, true);
-                return newIndex;
             }
             else
             {
                 EnableNextComboBox(boxIndex, false);
-                return 0;
             }
         }
 
+        /// <summary>
+        /// Called by WPF when a combo box selection changes
+        /// </summary>
         private void WeaponSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (_loading)
---
+++ Liberty/HReach/WeaponEditing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Liberty.Reach
+{
+    public sealed class WeaponEditing
+    {
+        /// <summary>
+        /// Returns a UIElement/Util.IAmmoDisplay that can be used to accurately display the weapon's ammo.
+        /// </summary>
+        /// <param name="saveData">The save data to read from</param>
+        /// <param name="weapon">The weapon object to display</param>
+        /// <param name="name">The name of the weapon</param>
+        /// <returns>A UIElement/Util.IAmmoDisplay corresponding to the weapon</returns>
+        public static UIElement GetAmmoDisplay(Reach.CampaignSave saveData, Reach.WeaponObject weapon, string name)
+        {
+            name = name.ToLower();
+
+            UIElement display;
+            if (name.Contains("plasma") || name.Contains("energy") || name.Contains("laser") || name.Contains("focus"))
+                display = new plasmaAmmoDisplay(weapon);
+            else if (name.Contains("target") || name.Contains("locator"))
+                display = new targetLocatorDisplay(saveData);
+            else
+                display = new regularAmmoDisplay(weapon);
+
+            return display;
+        }
+    }
+}
